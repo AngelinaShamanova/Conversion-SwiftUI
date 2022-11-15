3a999eccd3fe9c13531c1ebbbbb2c996434657f2
AngelinaShamanova <shamanovaaaa@yandex.ru>
2022-11-15 21:15:34 +0100

    Initial Commit

---
+++ ConversionChallenge1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConversionChallenge1
+//
+//  Created by Ангелина Шаманова on 15.11.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ConversionChallenge1/ConversionChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionChallenge1App.swift
+//  ConversionChallenge1
+//
+//  Created by Ангелина Шаманова on 15.11.22..
+//
+
+import SwiftUI
+
+@main
+struct ConversionChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
